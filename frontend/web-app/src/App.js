6afa6cb1005d9acb760cb64a1954e2040946e6c2
Suprabhat Rijal <goodguysman@gmail.com>
2022-10-01 15:24:31 -0400

    Initial commit

--- frontend/web-app/src/App.js
+++ frontend/web-app/src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import {React, useRef, useState, useEffect} from 'react'
 
 function App() {
+
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [isDrawing, setIsDrawing] = useState()
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    canvas.width = window.innerWidth * 2
+    canvas.height = window.innerHeight *2
+    canvas.style.width = `${window.innerWidth * 2}px`
+    canvas.style.height =`${window.innerHeight * 2}px`
+
+
+    const context = canvas.getContext('2d')
+
+    context.scale(2,2)
+    context.lineCap ="round"
+    context.strokeStyle = "black"
+    context.lineWidth = 2
+
+    contextRef.current = context
+  },[])
+
+  const startDrawing = ({nativeEvent}) => {
+    const {offsetX, offsetY} = nativeEvent
+
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX, offsetY)
+    setIsDrawing(true)
+    
+  }
+
+  const endDrawing = () => {
+    contextRef.current.closePath()
+    setIsDrawing(false)
+  }
+
+  const draw = ({nativeEvent}) => {
+    if (!isDrawing) {
+      return
+    }
+    const {offsetX, offsetY} = nativeEvent
+    contextRef.current.lineTo(offsetX, offsetY)
+    contextRef.current.stroke()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Canvas">
+      <canvas
+        ref={canvasRef}
+        onMouseDown={startDrawing}
+        onMouseUp={endDrawing}
+        onMouseMove={draw}
+      />
+      
     </div>
   );
 }
