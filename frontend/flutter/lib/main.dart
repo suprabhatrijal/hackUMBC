626343330cd4cc595672fdda7c400f5da3286ac0
arajshah <araj.shah@bison.howard.edu>
2022-10-02 01:59:26 -0400

    Flutter prototype

---
+++ frontend/flutter/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:drawing_app/providers/bg_color_provider.dart';
+import 'package:drawing_app/providers/eraser_provider.dart';
+import 'package:drawing_app/providers/pen_type_provider.dart';
+import 'package:drawing_app/providers/sheetnumber_provider.dart';
+import 'package:drawing_app/providers/sheets_provider.dart';
+import 'package:drawing_app/ui/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:url_strategy/url_strategy.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle());
+  setPathUrlStrategy();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+  ]);
+  // if (kIsWeb) {
+  //   // Increase Skia cache size to support bigger images.
+  //   const int megabyte = 1000000;
+  //   SystemChannels.skia.invokeMethod('Skia.setResourceCacheMaxBytes', 512 * megabyte);
+  //   // TOD: cant' await on invokeMethod due to https://github.com/flutter/flutter/issues/77018  so awaiting on Future.delayed instead.
+  //   await Future<void>.delayed(Duration.zero);
+  // }
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<dynamic> snapshot,
+      ) {
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider.value(
+              value: SheetsViewProvider(),
+            ),
+            ChangeNotifierProvider.value(
+              value: BgColorProvider(),
+            ),
+            ChangeNotifierProvider.value(
+              value: PenEraserProvider(),
+            ),
+            ChangeNotifierProvider.value(
+              value: SheetNumberProvider(),
+            ),
+            ChangeNotifierProvider.value(
+              value: EraserProvider(),
+            ),
+          ],
+          child: MaterialApp(
+            title: 'DrawingBoard',
+            home: DrawScreen(),
+            debugShowCheckedModeBanner: false,
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ frontend/flutter/lib/ui/home_page.dart
@@ -0,0 +1,163 @@
+import 'package:drawing_app/models/pen_stroke_model.dart';
+import 'package:drawing_app/providers/bg_color_provider.dart';
+import 'package:drawing_app/providers/eraser_provider.dart';
+import 'package:drawing_app/providers/sheetnumber_provider.dart';
+import 'package:drawing_app/providers/sheets_provider.dart';
+import 'package:drawing_app/ui/constants/constants.dart';
+import 'package:drawing_app/ui/painters/draw.dart';
+import 'package:drawing_app/ui/components/left_appbar.dart';
+import 'package:drawing_app/ui/components/right_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:provider/provider.dart';
+
+class DrawScreen extends StatefulWidget {
+  @override
+  _DrawScreenState createState() => _DrawScreenState();
+}
+
+class _DrawScreenState extends State<DrawScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Builder(
+        builder: (BuildContext context) => SafeArea(
+          child: Stack(
+            children: [
+              Consumer<SheetsViewProvider>(
+                builder: (context, sheetView, child) {
+                  return sheetView.isGrid
+                      ? GridPaper(
+                          interval: 20,
+                          divisions: 1,
+                          subdivisions: 1,
+                          child: Consumer<SheetNumberProvider>(
+                            builder: (context, sheetNProv, child) {
+                              return Consumer<EraserProvider>(
+                                builder: (context, eraseProv, child) {
+                                  print('ERASER CHANGED');
+                                  return Consumer<BgColorProvider>(
+                                    builder: (BuildContext context, bg,
+                                            Widget child) =>
+                                        Container(
+                                      color: bg.bgColor,
+                                      child: MouseRegion(
+                                        cursor: eraseProv.isEraser
+                                            ? SystemMouseCursors.copy
+                                            : SystemMouseCursors.basic,
+                                        child: GestureDetector(
+                                          onPanUpdate:
+                                              (DragUpdateDetails details) {
+                                            setState(
+                                              () {
+                                                RenderBox object =
+                                                    context.findRenderObject();
+                                                Offset _localPosition =
+                                                    object.globalToLocal(
+                                                        details.globalPosition);
+                                                PenStroke _localPoint =
+                                                    PenStroke();
+                                                _localPoint.color =
+                                                    eraseProv.isEraser
+                                                        ? bg.bgColor
+                                                        : brushColor;
+                                                _localPoint.offset =
+                                                    _localPosition;
+                                                _localPoint.brushWidth =
+                                                    eraseProv.isEraser
+                                                        ? eraserWidth
+                                                        : brushWidth;
+                                                _localPoint.strokeCap =
+                                                    strokeCap;
+                                                points = List.from(points)
+                                                  ..add(_localPoint);
+                                              },
+                                            );
+                                          },
+                                          onPanEnd: (DragEndDetails details) =>
+                                              {
+                                            deletedPoints.clear(),
+                                            points.add(null),
+                                            print('POINTS: ${points.length}'),
+                                          },
+                                          child: CustomPaint(
+                                            painter: DrawPen(points: points),
+                                            size: Size.infinite,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                          ),
+                        )
+                      : Consumer<SheetNumberProvider>(
+                          builder: (context, sheetNProv, child) {
+                            print(
+                                'NUMBER OF SHEETS 2: ${sheetNProv.sheetNumber}');
+
+                            return Consumer<EraserProvider>(
+                              builder: (context, eraseProv, child) =>
+                                  Consumer<BgColorProvider>(
+                                builder: (context, bg, child) => Container(
+                                  color: bg.bgColor,
+                                  child: MouseRegion(
+                                    cursor: eraseProv.isEraser
+                                        ? SystemMouseCursors.disappearing
+                                        : SystemMouseCursors.basic,
+                                    child: GestureDetector(
+                                      onPanUpdate: (DragUpdateDetails details) {
+                                        setState(
+                                          () {
+                                            RenderBox object =
+                                                context.findRenderObject();
+                                            Offset _localPosition =
+                                                object.globalToLocal(
+                                                    details.globalPosition);
+                                            PenStroke _localPoint = PenStroke();
+                                            _localPoint.color =
+                                                eraseProv.isEraser
+                                                    ? bg.bgColor
+                                                    : brushColor;
+                                            _localPoint.offset = _localPosition;
+                                            _localPoint.brushWidth =
+                                                eraseProv.isEraser
+                                                    ? eraserWidth
+                                                    : brushWidth;
+                                            _localPoint.strokeCap = strokeCap;
+                                            points = List.from(points)
+                                              ..add(_localPoint);
+                                          },
+                                        );
+                                      },
+                                      onPanEnd: (DragEndDetails details) => {
+                                        deletedPoints.clear(),
+                                        points.add(null),
+                                      },
+                                      child: CustomPaint(
+                                        painter: DrawPen(points: points),
+                                        size: Size.infinite,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                },
+              ),
+              TopAppBar(),
+              Positioned(
+                right: 0.0,
+                child: RightBar(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
